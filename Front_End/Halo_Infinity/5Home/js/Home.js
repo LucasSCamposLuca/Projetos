0388065d1c607ab327f5519a7ea0a4e4d652d511
LucasSCamposLuca <lucassouza104840@gmail.com>
2025-06-06 07:09:13 -0300

    Modificando Halo Infinity

---
+++ Front_End/Halo_Infinity/5Home/js/Home.js
@@ -0,0 +1,41 @@
+const dropdowns = document.querySelectorAll(`.dropdown`);
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector(`.select`);
+    const caret = dropdown.querySelector(`.caret`);
+    const menu = dropdown.querySelector(`.menu`);
+    const options = dropdown.querySelectorAll(`.menu li`);
+    const selected = dropdown.querySelector(`.selected`);
+
+    select.addEventListener(`click`, () => {
+        select.classList.toggle(`select-clicked`);
+        caret.classList.toggle(`caret-rotate`);
+        menu.classList.toggle(`menu-open`);
+    });
+    
+    options.forEach(option => {
+      option.addEventListener(`click`, () => {
+        selected.innerText = option.innerText;
+    
+        select.classList.remove(`select-clicked`);
+        caret.classList.remove(`caret-rotate`);
+        menu.classList.remove(`menu-open`);
+    
+        options.forEach(option => {
+            option.classList.remove(`active`);
+        });
+    
+        option.classList.add(`active`);
+        });
+    });
+});
+
+var tabela = $(".tabela");
+
+// tabela.on(`mouseover`, function (){
+//     tabela.animate({with: "100%", height: "100%", borderRadius: "100%"});
+// });
+
+//  tabela.on(`mouseleave`, function (){
+//      tabela.animate({with: "20rem", height: "auto", borderRadius: "20px"});
+// });
